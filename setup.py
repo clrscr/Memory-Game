c8040701dc411b47fbed0af05ee479d2b734b62b
Madmedic <1217270+clrscr@users.noreply.github.com>
2024-07-15 16:50:14 -0500

    Started packaging work

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Memory-Game',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='mad',
+    author_email='',
+    description=''
+)
